1592834b44943278894f165ae195e2d948b66da7
JH <jqhu@uw.edu>
2025-02-19 22:12:40 +0000

    logger instead of print

--- model/glmhmm/JH_model_fitting.py
+++ model/glmhmm/JH_model_fitting.py
@@ -16,6 +16,9 @@ import tamagotchi.eval.log_analysis as log_analysis
 
 import hydra
 from omegaconf import OmegaConf, open_dict
+import logging 
+log = logging.getLogger(__name__)
+
 
 # create time lag history on features of interest, for each group_column
 def create_history(df, feature_names, n_timesteps):
@@ -85,13 +88,13 @@ def parse_args():
     args.eval_folder = [os.path.dirname(eval_pkl) + '/' for eval_pkl in args.eval_pkl]
     # TODO: what if multiple files are not from the same seed? How to name then>? 
     args.model_name = os.path.basename(os.path.dirname(args.eval_folder[0])).split('_')[1]
-    print(f"now fitting {args.K} state on {args.model_name}, {args.dataset[0]} seed {args.seed}")
+    log.info(f"now fitting {args.K} state on {args.model_name}, {args.dataset[0]} seed {args.seed}")
     if not args.ID:
         args.out_fname = f"HMMGLM_seed{args.model_name}_randSeed{args.seed}_K{args.K}.npz"
     else:
         args.out_fname = f"HMMGLM_seed{args.model_name}_{args.ID}_randSeed{args.seed}_K{args.K}.npz"
     args.out_path = os.path.join(args.eval_folder[0], args.out_fname)
-    print(f"results will be saved to {args.out_path}")
+    log.info(f"results will be saved to {args.out_path}")
     
     return args
 
@@ -103,10 +106,10 @@ def main(args:OmegaConf):
         for k in args.keys(): 
             if k not in ['exp_name', 'comment']: # these are not nested
                 args = OmegaConf.merge(args, args[k])
-    print(args["comment"])
-    print(args)
+    log.info(args["comment"])
+    log.info(args)
     np.random.seed(args.seed)
-    print('seed:', args.seed)
+    log.info('seed:', args.seed)
 
     # load traj and neural activity data
     last_file_num_trials = 0
@@ -118,12 +121,11 @@ def main(args:OmegaConf):
         n_trials = args.n_trials[i]
         with open(obs_fname, 'rb') as f_handle:
             observability_tupl = pickle.load(f_handle)
-            print(len(observability_tupl))
         with open(eval_fname, 'rb') as f_handle:
             # based on open_loop_perturbation.py
             selected_df = log_analysis.get_selected_df(eval_folder, [dataset],
-                                                    n_episodes_home=240,
-                                                    n_episodes_other=240,  
+                                                    n_episodes_home=n_trials,
+                                                    n_episodes_other=0,  
                                                     balanced=False,
                                                     oob_only=False,
                                                     verbose=True,
@@ -134,13 +136,10 @@ def main(args:OmegaConf):
                                                                                                         obtain_traj_df = True, 
                                                                                                         get_traj_tmp = True,
                                                                                                         extended_metadata = True) # get_traj_tmp 
-            print(traj_df_stacked.shape)
-            print(stacked_neural_activity.shape)
 
         
         if args.filter_by_observability_trials:
             ls_EV_no_nan, ls_t_sim, ls_x_sim, ls_window_size, ls_eps_idx = zip(*observability_tupl)
-            print(ls_eps_idx)
             # Preprocess the trajectory data
             # select episodes that have observability matrices
             eps_at = [True if ep_i in ls_eps_idx else False for ep_i in traj_df_stacked['ep_idx'] ]
@@ -152,7 +151,7 @@ def main(args:OmegaConf):
         # for every episode, drop the last row
         subset_traj_df_stacked.reset_index(drop=True, inplace=True)
         last_rows = subset_traj_df_stacked.groupby('ep_idx').tail(1).index
-        print('dropping', len(last_rows), 'rows')
+        log.info('dropping', len(last_rows), 'rows')
         # drop the last row of each episode
         tmp_filtered_df = subset_traj_df_stacked.drop(index=last_rows)
         tmp_filtered_neural_activity = np.delete(subset_stacked_neural_activity, last_rows, axis=0)
@@ -174,22 +173,22 @@ def main(args:OmegaConf):
         else:
             filtered_df = pd.concat([filtered_df, tmp_filtered_df], ignore_index=True)
             filtered_neural_activity = np.concatenate([filtered_neural_activity, tmp_filtered_neural_activity], axis=0)
-        print("tmp_filtered_df shape", tmp_filtered_df.shape)
-        print("tmp_filtered_neural_activity shape", tmp_filtered_neural_activity.shape)
+        log.info("tmp_filtered_df shape", tmp_filtered_df.shape)
+        log.info("tmp_filtered_neural_activity shape", tmp_filtered_neural_activity.shape)
 
-    print("filtered_df shape", filtered_df.shape)
-    print("filtered_neural_activity shape", filtered_neural_activity.shape)
+    log.info("filtered_df shape", filtered_df.shape)
+    log.info("filtered_neural_activity shape", filtered_neural_activity.shape)
 
     # TODO load multiple EV datasets - not considering for now
     # # Preprocess the EV data 
     # # stack the EV data
     # ls_EV_no_nan = [df.assign(ep_idx=ep_idx) for df, ep_idx in zip(ls_EV_no_nan, ls_eps_idx)]
     # EV_no_nan = pd.concat(ls_EV_no_nan)
-    # print(EV_no_nan.shape)
+    # log.info(EV_no_nan.shape)
     # # Merge with filtered_dfa
     # EV_no_nan['time'] = EV_no_nan['time'].round(2)
     # EV_no_nan = EV_no_nan.merge(filtered_df[['ep_idx', 'time', 'time_since_last_wind_change', 'odor_01']], on=['ep_idx', 'time'], how='inner')
-    # print(EV_no_nan.shape)
+    # log.info(EV_no_nan.shape)
 
     # Preprocess action data
     # get speed and acceleration
@@ -201,7 +200,7 @@ def main(args:OmegaConf):
     obs_df['acceleration'] = obs_df.groupby('ep_idx')['speed'].diff()
     obs_df['turn'] = filtered_df['turn']
     obs_df['turn_velocity'] = ((obs_df['turn']  - 0.5)*2) * (6*np.pi) # in rad/s unit
-    print("range of turn is ", obs_df['turn'].min(), obs_df['turn'].max())
+    log.info("range of turn is ", obs_df['turn'].min(), obs_df['turn'].max())
     # obs_df['acceleration'] = obs_df['acceleration'].fillna(0) # TODO check timing 
 
     # calculate angular velocity and angular acceleration
@@ -216,8 +215,7 @@ def main(args:OmegaConf):
     obs_df['train_test_label'] = 'train'
     obs_df.loc[obs_df['ep_idx'].isin(test_idx), 'train_test_label'] = 'test'
     # print how many train and test episodes
-    print(obs_df.groupby('train_test_label')['ep_idx'].nunique())
-    print(obs_df[obs_df['train_test_label']=='test'].groupby('train_test_label')['ep_idx'].unique())
+    log.info(f"Looking at {obs_df.groupby('train_test_label')['ep_idx'].nunique()} unique episodes")
 
     input_df = pd.DataFrame()
     # observations of the agent
@@ -244,8 +242,8 @@ def main(args:OmegaConf):
 
     # set 10% trials as test set
     input_df['train_test_label'] = obs_df['train_test_label']
-    print(input_df.groupby('train_test_label')['ep_idx'].nunique())
-    print(obs_df[obs_df['train_test_label']=='test'].groupby('train_test_label')['ep_idx'].unique())
+    log.info(input_df.groupby('train_test_label')['ep_idx'].nunique())
+    log.info(f"Test episodes: {obs_df[obs_df['train_test_label']=='test'].groupby('train_test_label')['ep_idx'].unique()}")
     train_idx = input_df[input_df['train_test_label']=='train']['ep_idx'].unique()
 
     # create history of features by episode
@@ -283,7 +281,7 @@ def main(args:OmegaConf):
     N=X.shape[0] # length of training data
     m = glmhmm.GLMHMM(N, args.K, D, dim_output, covar_epsilon)
     m.optimizer_tol = args.tolerance
-    print('fitting model with tolerance (default if none):', m.optimizer_tol)
+    log.info('fitting model with tolerance (default if none):', m.optimizer_tol)
 
     A_init=m.transition_matrix
     w_init=m.w
@@ -294,7 +292,7 @@ def main(args:OmegaConf):
 
     lls_pred,A_pred,w_pred,pi0_pred = m.fit(Y,X,A_init,w_init, pi0=pi0_init, fit_init_states=True, 
                                             sess=None)
-    print('time taken:', time.time()-start)
+    log.info('time taken:', time.time()-start)
 
     np.savez(args.out_path, A_init=A_init, w_init=w_init, pi0_init=pi0_init,
             A_pred=A_pred, w_pred=w_pred, pi0_pred=pi0_pred, train_idx=train_idx, test_idx=test_idx,
